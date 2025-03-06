13ab7e7a0ab07cc74ed824a9546d2fb9e0e3aaab
Aidan <acm289@cornell.edu>
2025-03-06 11:10:24 -0500

    Project script

--- scripts/setup/create-project.tcl
+++ scripts/setup/create-project.tcl
@@ -10,3 +10,6 @@ create_project vivado-walkthrough . -part xc7z010clg400-1
 
 # Identify the exact board (not just the part)
 set_property board_part digilentinc.com:zybo-z7-10:part0:1.0 [current_project]
+
+# Open the GUI once done
+start_gui
